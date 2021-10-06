a10a7efa749267ff5c1242d307cf49fa5b088310
Christina2021 <christina.shiroma.01@gmail.com>
2021-10-06 11:36:00 -0500

    Added necessary pages; added hashrouter with routes elements.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Splash from './pages/Splash.js';
+import AboutMe from './pages/AboutMe';
+import Portfolio from './pages/Portfolio';
+import ContactMe from './pages/ContactMe';
+import NoMatch from './pages/NoMatch';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Splash} />
+          <Route exact path="/about" component={AboutMe} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/contact" component={ContactMe} />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Splash.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Splash() {
+    return(
+        <div>
+            <h1>Christina Shiroma</h1>
+            <p>Full stack software developer</p>
+            <a>About Me</a>
+            <a>Portfolio</a>
+            <a>Contact Me</a>
+        </div>
+    )
+}
+
+export default Splash;
